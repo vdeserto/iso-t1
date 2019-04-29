c459f6d53f6b7654a11514aad4c6afe71b2bc3b4
adrian-deserto-iso-t1 <50108858+adrian-deserto-iso-t1@users.noreply.github.com>
2019-04-29 11:05:07 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "commands.h"
+
+#define MAX_INPUT 512
+#define MAX_MYPS1 64
+
+char[MAX_MYPS1] MYPS1 = "iso$";
 
 int main(int argc, char const *argv[])
 {
-	char[MAX] input;
-	do{
+	char[MAX_INPUT] input;
 	
+	do{
+		myps1();
+		create_process(scanf("%s"));
 
-		
-	}while(strcmp(input,"exit") != 0)
+	}while(TRUE);
 	return 0;
 }
