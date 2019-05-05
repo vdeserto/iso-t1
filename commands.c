aac691c04c0a0f55380dd8f84cf42a27a5ec0773
thesanta69 <50109280+thesanta69@users.noreply.github.com>
2019-05-05 15:27:28 -0400

    Add files via upload

--- commands.c
+++ commands.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
+#include <strings.h>
 
 #include "commands.h"
 
@@ -42,25 +44,29 @@ void try_exec(char* string){
 
 
 void create_process (char* string){
-	Pid_t pid;
+	pid_t pid;
 	pid = fork();
+    int i, tam;
+
 	if(pid < 0){
 		fprintf(stderr, "%s\n", "Error");
 		exit(-1);
 	}
+
 	else if(pid == 0) /*processo filho */
 		try_exec(string);
+
 	else{/*processo pai */
-		for(int i=0; string[i] == '&' || string[i] != '\0'; i++){
+        tam = strlen(string);
+		if(string[tam-1] == '&')
 			wait(NULL);
 		}
-		
+
 		exit(0);
 	}
-}
 
 void try_exec(char* string){
-	
-
-
+    char * argv_list[] = {"ls","-lart","/home",NULL};
+    execv("ls",argv_list);
+    exit();
 }
--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 int main(int argc, char const *argv[])
 {
 	char[MAX_INPUT] input;
-	
+
 	do{
 		myps1();
 		create_process(scanf("%s"));
@@ -24,21 +24,50 @@ int main(int argc, char const *argv[])
 
 //BUSCA E TENTAR EXEC()
 
-	
+
 
 
 int main(void)
 {
 	char input[MAX_INPUT];
+	FILE *arquivo;
 
-	/*do{
+	do{
 		scanf("%s", input);
-		create_process(input);
 
-	}while(1); */
+		//Separar os argumentos
+		i=0;
+		j=0;
+		contador=0;
+		while(input[i] != '/n'){
+            if(input[i] != '/0'){
+                argumento[j] = input[i];
+                i++;
+                j++;
+            }
+            else{
+                contador++;
+                v[contador][0] = input[i];
+                v[contador][1] = input[i+1];
+                i=i+2;
+                j=0;
+            }
+		}
+        if(contador == 0)
+            create_process(input);
+        else if(contador == 1)
+            create_process(input, v[0]);
+        else if(contador == 2)
+            create_process(input, v[0], v[1]);
+        else if(contador == 3)
+            create_process(input, v[0], v[1], v[2]);
+        else if(contador == 4)
+            create_process(input, v[0], v[1], v[2], v[3]);
+
+	}while(1);
 
 	printf("%s", $PATH);
 
-	
+
 	return 0;
 }
